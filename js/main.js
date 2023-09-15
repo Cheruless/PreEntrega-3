6c8b6e00b4ecb649b56a0e26c0b16dfdfeb86b48
Exequiel <exepulgar@gmail.com>
2023-09-14 22:23:16 -0300

    Error en un if al validar si es mayor

--- js/main.js
+++ js/main.js
@@ -107,7 +107,7 @@ formLogin.onsubmit = e => {
       const inUserValue = document.querySelector('#inUser').value;
       const inPasswordValue = document.querySelector('#inPassword').value;
 
-      if (esMayor(inDate)) {
+      if (esMayor(inDateValue)) {
         const usuario = new Usuario(inUserValue, inPasswordValue, inDateValue);
         guardarUsuario(usuario)
       }
